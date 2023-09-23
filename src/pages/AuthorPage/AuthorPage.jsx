710bc7ad57838106dde6205e68b00ff6ecadcac3
fady jan <fady.jan95@gmail.com>
2023-09-24 01:23:10 +0300

    Create #15 Author Page

---
+++ src/pages/AuthorPage/AuthorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import module from './AuthorPage.module.css'
+import TableComponent from "../../components/Table/Table";
+
+const AuthorPage = () => {
+  return (
+    <TableComponent></TableComponent>
+  )
+}
+
+export default AuthorPage
