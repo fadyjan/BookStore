abd60990d35408a659780914191904f0cd46f9dd
fady jan <fady.jan95@gmail.com>
2023-09-24 01:20:04 +0300

    Create #5 Entry Points

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { SideBar } from './components/SideBar/SideBar'
+import SelectedLayout from './layout/SelectedLayout/SelectedLayout'
+function App() {
+
+  return (
+    <>
+    <SideBar></SideBar>
+    <SelectedLayout></SelectedLayout>
+    </>
+  )
+}
+
+export default App
