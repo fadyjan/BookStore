f1c5df3d6444ae31e551aaca85d15d30a26dc952
fady jan <fady.jan95@gmail.com>
2023-09-24 05:14:40 +0300

    Create #22 Add NewButton can now add New Author

--- src/components/AddNewButton/AddNewButton.jsx
+++ src/components/AddNewButton/AddNewButton.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import ModalPopUp from "../ModalPopUp/ModalPopUp";
 import module from "./AddNewButton.module.css";
-const AddNewButton = () => {
+import { HandleButtonSpan } from "../../store/ReduxSlices/SideBarSlice";
+const AddNewButton = ({ SelectedPage }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  let ButtonSpan = HandleButtonSpan(SelectedPage);
+
   const HandleShowPopUp = () => {
     setIsModalOpen(true);
   };
 
   return (
     <div>
-      <button onClick={HandleShowPopUp} id={module.ButtonWrapper}>Add New Book</button>
+      <button onClick={HandleShowPopUp} id={module.ButtonWrapper}>{"Add New "+ ButtonSpan}</button>
       {isModalOpen && (
         <ModalPopUp
           isModalOpen={isModalOpen}
