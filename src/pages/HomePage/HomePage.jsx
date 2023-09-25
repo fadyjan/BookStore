943a2765e91df82fabac4c7d875a01ce761b6718
fady jan <fady.jan95@gmail.com>
2023-09-25 19:33:07 +0300

    Enhance #11 HomePage Logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import module from "./HomePage.module.css";
 import BrowseBy from "../../components/BrowseBy/BrowseBy";
-import {useSelector} from "react-redux";
 
 const HomePage = () => {
   
